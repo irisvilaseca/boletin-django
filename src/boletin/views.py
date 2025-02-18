e5e668ea94c3c1784dff4d702b43a7f6996f2bdf
Iris Vilaseca <iris.vilaseca1998@gmail.com>
2025-02-18 12:24:36 +0100

    form saved and updated to Django last version

--- src/boletin/views.py
+++ src/boletin/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
-from .forms import RegForm
+from .forms import RegModelForm
 from .models import Registered
 # Create your views here.
 def index(request):
-    form=RegForm(request.POST or None)
+    title="Hello"
+    if request.user.is_authenticated:
+        title=f"Welcome {request.user}"
+    form=RegModelForm(request.POST or None)
     if form.is_valid():
-        form_data=form.cleaned_data
-        abc=form_data.get("email")
-        abc2=form.data.get("name")
-        obj=Registered.objects.create(email=abc,name=abc2)
+        instance=form.save(commit=False)
+        print(instance)
+        instance.save()
+        print(instance)
+        print(instance.timestamp)
+        #form_data=form.cleaned_data
+        #abc=form_data.get("email")
+        #abc2=form.data.get("name")
+        #obj=Registered.objects.create(email=abc,name=abc2)
     context={
+        "title":title,
         "form":form,
     }
     return render(request,"index.html",context)
